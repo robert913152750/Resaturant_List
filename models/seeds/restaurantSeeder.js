e05d50ced95074308d13701f0a97b3dfae76337f
Robert <robert913152750@gmail.com>
2020-03-24 18:29:18 +0800

    use mongoose to add seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Restaurant = require("../restauranrt");
+const restaurantList = require("./restaurant.json");
+mongoose.connect("mongodb://localhost/restaurant", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("db error");
+});
+
+db.once("open", () => {
+  console.log("db connected");
+  const list = restaurantList.results;
+  for (i = 0; i < restaurantList.results.length; i++) {
+    Restaurant.create({
+      name: list[i].name,
+      name_en: list[i].name_en,
+      category: list[i].category,
+      image: list[i].image,
+      location: list[i].location,
+      phone: list[i].phone,
+      google_map: list[i].google_map,
+      rating: list[i].rating,
+      description: list[i].description
+    });
+  }
+});
